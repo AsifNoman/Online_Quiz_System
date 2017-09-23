d28acdc5e723a0860e94cf275d66a223a79c6efa
Asif Al Noman <asifnoman240@gmail.com>
2017-09-24 00:17:13 +0600

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -1,31 +1,50 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login Page</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Log in</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/iCheck/square/blue.css">
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">  
 </head>
-<body>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>Login</b>Panel</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Sign Up to start your session</p>
 
-    <h1>Login Page</h1>
+    <form action="" method="post">
+      <div class="form-group has-feedback">
+        <input name="username" id="username" class="form-control" placeholder="User Name" type="text" required>
+        <span class="fa fa-user form-control-feedback" aria-hidden="true"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-4">
+          <input type="submit" name="login" class="btn btn-primary btn-block btn-flat" value="Sign In">
+        </div>
+      </div>
+    </form><br>
 
-    <p>Type Username and Password to login!!</p>
+    <p class="text-danger"><?php if (isset($message)) { echo $message; } ?> </p>
 
-    <form action="" method="POST">
+    <a href="<?php echo base_url();?>register" class="text-center">Register a new membership</a>
 
-            <label for="username">Username:</label><br>
-            <input name="username" id="username" type="text"><br><br>
-
-            <label for="password">Password</label><br>
-            <input name="password" id="password" type="password"><br><br>
-
-            <input type="submit" name="login" value="Login">
-
-    </form><br><br>
-
-
-Don't have an account !!!<a href="<?php echo base_url(); ?>register"> Register Now</a><br><br>
-
-         <?php if (isset($message)) { 
-         echo $message; } ?>
+  </div>
+</div>
 
 </body>
 </html>
